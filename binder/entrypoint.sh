590a72706f2f8613fdb3f6059e78e67dd3a16764
David Lange <David.Lange@cern.ch>
2025-11-19 18:53:19 +0100

    Update entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -11,5 +11,6 @@ unset __conda_setup
 conda activate "${_CONDA_DEFAULT_ENV}"
 # This just logs the output to stderr for debugging. 
 >&2 echo "ENTRYPOINT: CONDA_DEFAULT_ENV=${CONDA_DEFAULT_ENV}"
+>&2 conda list
 
 exec "$@"
