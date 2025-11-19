9818bccfba2700ead27390ed3ef0c43455d3c40b
David Lange <David.Lange@cern.ch>
2025-11-19 18:52:50 +0100

    Update entrypoint.sh

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
-set  -e
+#!/bin/bash
 
-conda activate hsf-india
-#source /opt/_activate_conda.sh
+# cache the value because the shell hook step will remove it
+_CONDA_DEFAULT_ENV="hsf-india"
+
+__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+eval "$__conda_setup"
+unset __conda_setup
+
+# Restore our "indended" default env
+conda activate "${_CONDA_DEFAULT_ENV}"
+# This just logs the output to stderr for debugging. 
+>&2 echo "ENTRYPOINT: CONDA_DEFAULT_ENV=${CONDA_DEFAULT_ENV}"
 
 exec "$@"
